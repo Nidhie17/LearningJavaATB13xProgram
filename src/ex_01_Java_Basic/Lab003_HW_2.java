467f556121f6be1b6b34a7376f55bddd9ba3639d
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-05 22:19:46 +0530

    File updated

--- src/ex_01_Java_Basic/Lab003_HW_2.java
+++ src/ex_01_Java_Basic/Lab003_HW_2.java
@@ -3,8 +3,7 @@ package ex_01_Java_Basic;
 public class Lab003_HW_2 {
     public static void main(String[] args) {
         System.out.println("nidhie"+"kumari");
-        System.out.println("nidhie"+"kumari");
-        System.out.println("nidhie"+"kumari");
+
         System.out.println("nidhie"+"kumari");
     }
 }
