7c2485546c457f67efd23b902434759e90b9d7e5
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-15 17:41:43 +0530

    ammend

---
+++ src/ex_01_Java_Basic/lab004_HelloworldProgram.java
@@ -0,0 +1,6 @@
+
+public class lab004_HelloworldProgram{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
---
+++ src/ex_01_Java_Basic/lab005_namePrint.java
@@ -0,0 +1,8 @@
+
+public class lab005_namePrint {
+
+	public static void main(String[] args) {
+		System.out.println("Nidhi");
+	}
+
+}
