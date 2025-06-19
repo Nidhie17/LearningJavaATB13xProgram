50314a98359f702c65943731a0c335deff1c627c
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-19 07:51:33 +0530

    Test-19june

---
+++ src/Test_19june/Assignment_Proper_declaration_Variable.java
@@ -0,0 +1,29 @@
+package Test_19june;
+
+public class Assignment_Proper_declaration_Variable {
+    public static void main(String[] args) {
+        //Declare and initialize variables of different primitive data types (int, double, boolean, char
+        byte Byte= 10;
+        int Integer=10;
+        short Short=10;
+        long Long= 123456789098L;
+        float Float=3.111f;
+        double Double=67.888888888888888;
+        boolean Boolean= true;
+        char CHAR='A';
+
+        //- Print the values of all variables
+        System.out.printf("%d is value for byte \n",Byte);
+        System.out.printf("%d is value for integer \n",Integer);
+        System.out.printf("%d is value for short \n", Short);
+        System.out.printf("%d is value for long \n",Long);
+        System.out.printf("%f is value for float \n",Float);
+        System.out.printf("%fis value for double \n",Double);
+        System.out.printf("%b is value for boolean \n",Boolean);
+        System.out.printf("%s is value for char \n",CHAR);
+
+
+
+
+    }
+}
