a4027cdc4b4e50654ec6a55140f38033d1d1217d
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-19 08:19:36 +0530

    Test-19june

---
+++ src/Test_19june/Logical_comparison_operator.java
@@ -0,0 +1,19 @@
+package Test_19june;
+
+public class Logical_comparison_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a==b);
+        System.out.println(a>b);
+        System.out.println(a < b);
+        System.out.println(a > b && a>0);
+        System.out.println(a > b || a>0);
+        System.out.println(!(a>b));
+
+
+        }
+    }
+
+
