8cd0922b7cccb6dbb8dcea2c38d7dead46f273a4
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-19 08:05:47 +0530

    Test-19june

---
+++ src/Test_19june/Assignment_Arithmetic_Operator.java
@@ -0,0 +1,18 @@
+package Test_19june;
+
+public class Assignment_Arithmetic_Operator {
+    public static void main(String[] args) {
+        int a=10;
+      /*  assignment operator-RHV to the LH
+        int is a Data type
+        a and b is identifier
+        equal symbol is assignment operator
+        */
+       int b=5;
+        System.out.printf("Perform Addition : %d\n",a+b);
+        System.out.printf("Perform Substraction :%d\n",a-b);
+        System.out.printf("Perform Multiplication :%d\n",a*b);
+        System.out.printf("Perform Division :%d\n",a/b);
+        System.out.printf("Perform Modulus :%d\n",a%b);
+    }
+}
