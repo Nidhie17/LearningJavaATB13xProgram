49c826d4ff2a7455a147293d032a677460687a12
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-19 08:22:50 +0530

    Test-19june

---
+++ src/Test_19june/Increment_decrement.java
@@ -0,0 +1,14 @@
+package Test_19june;
+
+public class Increment_decrement {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+    }
+}
