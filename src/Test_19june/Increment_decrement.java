cbaa6351b319c96885e27ba7f2bc08b25863f993
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-24 12:55:55 +0530

    Test

--- src/Test_19june/Increment_decrement.java
+++ src/Test_19june/Increment_decrement.java
@@ -4,11 +4,12 @@ public class Increment_decrement {
     public static void main(String[] args) {
 
 //increment and decrement
-        int a = 10;
+        int a = 5;
+        System.out.printf("Original: %d\n",a);
 
-        System.out.println(a++);
-        System.out.println(++a);
-        System.out.println(a--);
-        System.out.println(--a);
+        System.out.printf("Pre-increment : %d\n",++a);
+        System.out.printf("Post-increment : %d (returned %d)\n",a++,a);
+        System.out.printf("Pre-decrement : %d\n",--a);
+        System.out.printf("Post-decrement : %d (returned %d)\n",a--,a);
     }
 }
