696f33e35309eaea50b4f5339ff3ef150d386ac9
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-26 08:15:44 +0530

    string test

---
+++ src/stringtest/ex_01.java
@@ -0,0 +1,19 @@
+package stringtest;
+
+public class ex_01 {
+    public static void main(String[] args) {
+        String name= "Hello World";
+
+        System.out.printf("Length: %d\n", name.length() );
+
+        System.out.printf("Firstname : %s\n",name.charAt(0));
+
+        System.out.println(name.substring(6, 11));
+
+
+
+
+
+
+    }
+}
