5d373d18ef15e189cf4a028144a70fd231f83ae1
Nidhie17 <nidhiekumari.jnu@gmail.com>
2025-06-30 20:02:07 +0530

    Code practice-30june

---
+++ src/ex_17_Oops/Lab166_Oops.java
@@ -0,0 +1,9 @@
+package ex_17_Oops;
+
+public class Lab166_Oops {
+    public static void main(String[] args) {
+        Person amit = new Person();
+        Person pramod = new Person();
+
+    }
+}
